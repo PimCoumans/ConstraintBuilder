7882d3dc82149cb856df5d887ad882cbbb1c853c
Pim <pim@pixelrock.nl>
2022-08-19 17:21:19 +0200

    Very basic tests for my assumptions

--- Tests/ConstraintBuilderTests/ConstraintBuilderTests.swift
+++ Tests/ConstraintBuilderTests/ConstraintBuilderTests.swift
@@ -1,5 +1,23 @@
 import XCTest
-@testable import ConstraintBuilder
+import ConstraintBuilder
 
 final class ConstraintBuilderTests: XCTestCase {
+	#if canImport(UIKit)
+	
+	var superview: UIView!
+	var view: UIView!
+	override func setUp() {
+		superview = UIView()
+		view = UIView()
+		superview.addSubview(view)
+	}
+	
+	func testExtendToSuperview() {
+		view.extendToSuperview()
+	}
+	
+	func testCenterInSuperview() {
+		view.centerInSuperview()
+	}
+	#endif
 }
